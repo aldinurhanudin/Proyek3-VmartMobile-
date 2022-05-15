ce8c25cea62fc6d7f56a7832538e954688a90827
icntsy <icanatasya72@gmail.com>
2022-05-16 00:01:28 +0700

    shared preferences alamat baru

--- Vmart/login/lib/alamat_baru.dart
+++ Vmart/login/lib/alamat_baru.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AlamatBaru extends StatefulWidget {
+  // shared preference alamat baru
   static String tag = 'AlamatBaru';
   @override
   _AlamatBaruState createState() => _AlamatBaruState();
